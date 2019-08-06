f5e97e725128cc3dc111ae43ff3b4cecbe872e7a
tomasz-herman <tomasz.herman@protonmail.com>
2019-08-06 19:48:21 +0200

    args parsing

--- src/com/hermant/Main.java
+++ src/com/hermant/Main.java
@@ -4,10 +4,12 @@ import com.hermant.machine.Machine;
 import com.hermant.parser.Parser;
 import com.hermant.program.Program;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("PseudoAssembler Emulator version 2.19.11 beta");
+        System.out.println("PseudoAssembler Emulator version 2.19.12 beta");
         usage();
         Program program = Parser.parse(parseArgsForInputFile(args));
         Machine m = new Machine(parseArgsForDebug(args));
@@ -16,25 +18,25 @@ public class Main {
     }
 
     private static String parseArgsForInputFile(String[] args){
-        for (int i = 0; i < args.length; i++) {
-            if(args[i].equals("-i")||args[i].equals("--input"))
-                if(i<args.length-1)
-                    return args[i+1];
-                else{
-                    throw new IllegalStateException("no input file specified");
-                }
-        }
-        throw new IllegalStateException("Missing required option: -i");
+        for (String arg : args)
+            if (arg.startsWith("--input="))
+                return arg.substring(8);
+        System.out.println("Specify input file:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
     private static boolean parseArgsForDebug(String[] args){
         for (String arg : args)
-            if (arg.equals("-d") || arg.equals("--debug"))
+            if (arg.equals("--debug"))
                 return true;
         return false;
     }
 
     private static void usage(){
-        System.out.println("Usage:\njava -jar utility-name -i <input file> [--debug]");
+        System.out.println("Usage:\njava --enable-preview -jar PseudoAssemblerEmulator.jar <flags>");
+        System.out.println("Flags:");
+        System.out.println("--input=FILE\tspecify input file(optional, will ask for it if not specified)");
+        System.out.println("--debug\t\t\tenable debug(optional)");
     }
 }
