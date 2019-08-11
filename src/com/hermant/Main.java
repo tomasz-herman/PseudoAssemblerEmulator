b5ee83f99f70c74b108e2c4e256c22f49e6e6489
tomasz-herman <tomasz.herman@protonmail.com>
2019-08-11 11:11:53 +0200

    performance improvements

--- src/com/hermant/Main.java
+++ src/com/hermant/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        if(parseArgsForVersion(args))System.out.println("PseudoAssembler Emulator version 2.23.2 beta");
+        if(parseArgsForVersion(args))System.out.println("PseudoAssembler Emulator version 2.24.0 beta");
         if(parseArgsForHelp(args))usage();
         Program program;
         String input = parseArgsForInputFile(args);
