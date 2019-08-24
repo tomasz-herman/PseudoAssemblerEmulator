2ed64defc76f90372d7513340b83ad10698bd6d5
tomasz-herman <tomasz.herman@protonmail.com>
2019-08-24 03:16:18 +0200

    updated insertion sort

--- examples/insertion_sort.asm
+++ examples/insertion_sort.asm
@@ -1,9 +1,7 @@
 ;PROGRAM SORTUJĄCY TABLICĘ METODĄ INSERTION SORT
-ZERO:       DC      INTEGER(0)
-CZTERY:     DC      INTEGER(4)
-MAXVALUE:   DC      INTEGER(100)
-TAB:        DS      25*INTEGER
-TAB_SIZE:   DC      INTEGER(25)
+MAXVALUE:   DC      INTEGER(100000)
+TAB:        DS      16000*INTEGER
+TAB_SIZE:   DC      INTEGER(16000)
 SEPARATOR:  DC      CHAR('\n')
 
 MAIN:
@@ -77,39 +75,34 @@ L2:         IOUT    0(0)
 ;OPERUJE NA:
 ;REJESTRY 2, 3, 4, 5, 6
 ;WYMAGA DEKLARACJI:
-;CZTERY - INTEGER(4)
-;ZERO - INTEGER(0)
+;---
 ;ZWRACA:
 ;REJESTR 0 - WEJŚCIOWY POINTER NA TAB
 ;REJESTR 1 - WEJŚCIOWY ROZMIAR TABLICY
 INSERTION_SORT:
             PUSH    0
             PUSH    1
-            LD      2, ZERO ;R2: i = 1
-            INC     2
-L3:         CMP     1, 2
-            JLE     L3STOP
-            LD      3, 2    ;R3: j = i
-            LD      5, CZTERY
-            MUL     5, 2
-            LD      4, 0
-            ADD     4, 5
+            DEC     1
+            XOR     5, 5
+L3:
+            LDA     2, 0(0)
+            LDA     0, 0(4)
+            LD      4, 0(0)
+            LD      3, 11(0)
+            SUB     3, 1
 L4:
-            CMP     3, ZERO ;j >? 0
-            JLE     L4STOP
-            LD      5, 4
-            LDA     4, 4(-4)
-            LD      6, 4(0)
-            CMP     6, 5(0) ;A[j-1] >? A[j]
-            JLE     L4STOP
-            XCHG    6, 5(0)
-            XCHG    6, 4(0)
-            DEC     3
-            JMP     L4
-L4STOP:
-            INC     2
-            JMP     L3
-L3STOP:
+            CMP     3, 5
+            JL      BREAK
+            CMP     4, 2(0)
+            JGE     BREAK
+            LD      6, 2(0)
+            ST      6, 2(4)
+            LDA     2, 2(-4)
+            LOOP    3, L4
+BREAK:
+            ST      4, 2(4)
+            LOOP    1, L3
+STOP:
             POP     1
             POP     0
             RET
