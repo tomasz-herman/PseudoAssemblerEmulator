070e45cb81f348c45632051a3eb7cb736dab9eab
tomasz-herman <tomasz.herman@protonmail.com>
2019-09-11 20:04:40 +0200

    memory operation refactor

--- src/com/hermant/program/instruction/MemoryOperation.java
+++ src/com/hermant/program/instruction/MemoryOperation.java
@@ -5,8 +5,8 @@ import com.hermant.machine.register.GeneralPurposeRegister;
 public interface MemoryOperation {
 
     default int getMemoryAddress(GeneralPurposeRegister reg, int address, short addressOffset){
-        int indirect = reg.get(address);
-        short direct = (short)(indirect + addressOffset);
-        return indirect & 0xffff0000 | Short.toUnsignedInt(direct);
+        final int indirect = reg.get(address);
+        final int direct = indirect + addressOffset;
+        return indirect & 0xffff0000 | (direct & 0xffff);
     }
 }
