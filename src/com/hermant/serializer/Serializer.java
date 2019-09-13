53efc6a4d25c07716421d007b7acb9d30f6fd500
tomasz-herman <tomasz.herman@protonmail.com>
2019-09-13 11:09:09 +0200

    serializer refactor

--- src/com/hermant/serializer/Serializer.java
+++ src/com/hermant/serializer/Serializer.java
@@ -7,6 +7,7 @@ import com.hermant.program.instruction.LoadableInstruction;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.file.Files;
@@ -15,15 +16,67 @@ import java.nio.file.Paths;
 public class Serializer {
 
     public enum DeclarationType{
-        SINGLE_BYTE,
-        SINGLE_INT,
-        MANY_BYTES,
-        MANY_INTS,
-        STRING,
-        EMPTY_BYTE,
-        EMPTY_INT,
-        MANY_EMPTY_BYTES,
-        MANY_EMPTY_INTS
+        SINGLE_BYTE{
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new ByteDeclaration(1, buffer.get());
+            }
+        },
+        SINGLE_INT {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new IntegerDeclaration(1, buffer.getInt());
+            }
+        },
+        MANY_BYTES {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new ByteDeclaration(Short.toUnsignedInt(buffer.getShort()), buffer.get());
+            }
+        },
+        MANY_INTS {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new IntegerDeclaration(Short.toUnsignedInt(buffer.getShort()), buffer.getInt());
+            }
+        },
+        STRING {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                StringBuilder string = new StringBuilder();
+                int chars = Short.toUnsignedInt(buffer.getShort());
+                for (int i = 0; i < chars; i++) {
+                    string.append((char)buffer.get());
+                }
+                return new StringDeclaration(string.toString());
+            }
+        },
+        EMPTY_BYTE {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new ByteDeclaration(1, null);
+            }
+        },
+        EMPTY_INT {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new IntegerDeclaration(1, null);
+            }
+        },
+        MANY_EMPTY_BYTES {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new ByteDeclaration(Short.toUnsignedInt(buffer.getShort()), null);
+            }
+        },
+        MANY_EMPTY_INTS {
+            @Override
+            Declaration deserialize(ByteBuffer buffer) {
+                return new IntegerDeclaration(Short.toUnsignedInt(buffer.getShort()), null);
+            }
+        };
+
+        abstract Declaration deserialize(ByteBuffer buffer);
     }
 
     public static Program deserializeBinary(String path){
@@ -36,39 +89,17 @@ public class Serializer {
             int maxDeclarationType = DeclarationType.values().length - 1;
             while(declarations > 0 && buffer.hasRemaining()){
                 int typeIndex = buffer.get();
-                if(typeIndex > maxDeclarationType || typeIndex < 0) throw new SerializationException("invalid internal structure");
+                if(typeIndex > maxDeclarationType || typeIndex < 0)
+                    throw new SerializationException("invalid internal structure");
                 DeclarationType type = DeclarationType.values()[typeIndex];
-                switch(type){
-                    case SINGLE_BYTE -> program.addDeclaration(new ByteDeclaration(1, buffer.get()));
-                    case SINGLE_INT -> program.addDeclaration(new IntegerDeclaration(1, buffer.getInt()));
-                    case EMPTY_BYTE -> program.addDeclaration(new ByteDeclaration(1, null));
-                    case EMPTY_INT -> program.addDeclaration(new IntegerDeclaration(1, null));
-                    case MANY_BYTES -> program.addDeclaration(new ByteDeclaration(Short.toUnsignedInt(buffer.getShort()), buffer.get()));
-                    case MANY_INTS -> program.addDeclaration(new IntegerDeclaration(Short.toUnsignedInt(buffer.getShort()), buffer.getInt()));
-                    case MANY_EMPTY_BYTES -> program.addDeclaration(new ByteDeclaration(Short.toUnsignedInt(buffer.getShort()), null));
-                    case MANY_EMPTY_INTS -> program.addDeclaration(new IntegerDeclaration(Short.toUnsignedInt(buffer.getShort()), null));
-                    case STRING -> {
-                        StringBuilder string = new StringBuilder();
-                        int chars = Short.toUnsignedInt(buffer.getShort());
-                        for (int i = 0; i < chars; i++) {
-                            string.append((char)buffer.get());
-                        }
-                        program.addDeclaration(new StringDeclaration(string.toString()));
-                    }
-                }
+                program.addDeclaration(type.deserialize(buffer));
                 declarations--;
             }
             if(declarations > 0) throw new SerializationException("invalid internal structure");
             while(buffer.hasRemaining()){
-                byte code = buffer.get();
-                int instructionLength = InstructionFactory.getInstructionLength(code);
-                byte reg = buffer.get();
-                byte reg1 = (byte)(reg>>4&0xf);
-                byte reg2 = (byte)(reg&0xf);
-                short offset = instructionLength == 4 ? buffer.getShort() : 0;
-                program.addInstruction(new LoadableInstruction(code, reg1, reg2, offset));
+                program.addInstruction(deserializeInstruction(buffer));
             }
-        } catch (IOException | SerializationException e) {
+        } catch (IOException | SerializationException | BufferUnderflowException e) {
             e.printStackTrace();
             System.exit(1);
         }
@@ -87,5 +118,15 @@ public class Serializer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static LoadableInstruction deserializeInstruction(ByteBuffer buffer){
+        byte code = buffer.get();
+        int instructionLength = InstructionFactory.getInstructionLength(code);
+        byte reg = buffer.get();
+        byte reg1 = (byte)(reg>>4&0xf);
+        byte reg2 = (byte)(reg&0xf);
+        short offset = instructionLength == 4 ? buffer.getShort() : 0;
+        return new LoadableInstruction(code, reg1, reg2, offset);
     }
 }
