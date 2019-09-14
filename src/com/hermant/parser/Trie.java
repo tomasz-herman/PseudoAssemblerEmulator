79c731d022f644cb84d2dd21902ead0995c9e6db
tomasz-herman <tomasz.herman@protonmail.com>
2019-09-15 01:49:00 +0200

    Trie missing methods

--- src/com/hermant/parser/Trie.java
+++ src/com/hermant/parser/Trie.java
@@ -2,7 +2,7 @@ package com.hermant.parser;
 
 import java.util.*;
 
-class Trie<V> extends AbstractMap<String, V> implements Map<String, V> {
+public class Trie<V> extends AbstractMap<String, V> implements Map<String, V> {
 
     private static final int CHARS = 26;
 
@@ -10,6 +10,7 @@ class Trie<V> extends AbstractMap<String, V> implements Map<String, V> {
     private V value = null;
     private List<Trie<V>> children;
     private int size = 0;
+    private int parent;
 
     Trie() {
         children = new ArrayList<>(Collections.nCopies(CHARS, null));
@@ -37,6 +38,7 @@ class Trie<V> extends AbstractMap<String, V> implements Map<String, V> {
             if(children.get(index) == null)
                 children.set(index, new Trie<>());
             current = children.get(index);
+            current.parent = index;
         }
         if(current.hasValue){
             V old = current.value;
@@ -65,24 +67,50 @@ class Trie<V> extends AbstractMap<String, V> implements Map<String, V> {
 
     @Override
     public boolean containsKey(Object key) {
-        Trie<V> current = this;
-        if(!(key instanceof String))throw new IllegalArgumentException();
-        String k = (String)key;
-        for (int i = 0; i < k.length(); i++) {
-            current = current.children.get(k.charAt(i) - 'A');
-            if(current == null) return false;
-        }
-        return true;
+        return get(key) != null;
     }
 
     @Override
     public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException();
+        if(value.equals(this.value)) return true;
+        for (Trie<V> trie : children) {
+            if(trie == null) continue;
+            if(trie.containsValue(value)) return true;
+        }
+        return false;
     }
 
     @Override
     public V remove(Object key) {
-        throw new UnsupportedOperationException();
+        if(!(key instanceof String))throw new IllegalArgumentException();
+        String k = (String) key;
+        Trie<V> current = this;
+        final Stack<Trie<V>> path = new Stack<>();
+        for (int i = 0; i < k.length(); i++) {
+            path.add(current);
+            final int index = k.charAt(i) - 'A';
+            final List<Trie<V>> children = current.children;
+            if(children.get(index) == null)
+                return null;
+            current = children.get(index);
+        }
+        if(current.hasValue){
+            V old = current.value;
+            current.value = null;
+            current.size--;
+            current.hasValue = false;
+            while(!path.empty()){
+                Trie<V> trie = path.pop();
+                if(current.size == 0){
+                    trie.children.set(current.parent, null);
+                    trie.size--;
+                    current = trie;
+                }
+                else break;
+            }
+            return old;
+        }
+        return null;
     }
 
     @Override
