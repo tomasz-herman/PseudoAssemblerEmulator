5b784d4989ed27c403f7a01bd827d117dd9a1dbf
tomasz-herman <tomasz.herman@protonmail.com>
2019-09-14 23:50:51 +0200

    Trie now inherit Map interface

--- src/com/hermant/parser/Trie.java
+++ src/com/hermant/parser/Trie.java
@@ -1,42 +1,116 @@
 package com.hermant.parser;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-class Trie<V> {
+class Trie<V> extends AbstractMap<String, V> implements Map<String, V> {
 
     private static final int CHARS = 26;
 
     private boolean hasValue = false;
     private V value = null;
     private List<Trie<V>> children;
+    private int size = 0;
 
     Trie() {
         children = new ArrayList<>(Collections.nCopies(CHARS, null));
     }
 
-    void put(String key, V value){
+    @Override
+    public V get(Object key) {
         Trie<V> current = this;
+        if(!(key instanceof String))throw new IllegalArgumentException();
+        String k = (String)key;
+        for (int i = 0; i < k.length(); i++) {
+            current = current.children.get(k.charAt(i) - 'A');
+            if(current == null) return null;
+        }
+        return current.value;
+    }
+
+    public V put(String key, V value){
+        Trie<V> current = this;
+        final Stack<Trie<V>> path = new Stack<>();
         for (int i = 0; i < key.length(); i++) {
+            path.add(current);
             final int index = key.charAt(i) - 'A';
             final List<Trie<V>> children = current.children;
             if(children.get(index) == null)
                 children.set(index, new Trie<>());
             current = children.get(index);
         }
-        if(current.hasValue)throw new IllegalStateException("The key already exist in the trie.");
+        if(current.hasValue){
+            V old = current.value;
+            current.value = value;
+            return old;
+        }
+        while(!path.empty()){
+            Trie<V> trie = path.pop();
+            trie.size++;
+        }
+        current.size++;
         current.hasValue = true;
         current.value = value;
+        return null;
     }
 
-    V get(String key){
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
         Trie<V> current = this;
-        for (int i = 0; i < key.length(); i++) {
-            current = current.children.get(key.charAt(i) - 'A');
-            if(current == null) return null;
+        if(!(key instanceof String))throw new IllegalArgumentException();
+        String k = (String)key;
+        for (int i = 0; i < k.length(); i++) {
+            current = current.children.get(k.charAt(i) - 'A');
+            if(current == null) return false;
         }
-        return current.value;
+        return true;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends V> m) {
+        for(var entry : m.entrySet()) put(entry.getKey(), entry.getValue());
+    }
+
+    @Override
+    public void clear() {
+        children = new ArrayList<>(Collections.nCopies(CHARS, null));
+        size = 0;
+        hasValue = false;
+        value = null;
+    }
+
+    @Override
+    public Set<String> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<V> values() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Set<Entry<String, V>> entrySet() {
+        throw new UnsupportedOperationException();
     }
 
 }
