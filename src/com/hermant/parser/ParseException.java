a2e5f6f70efd1b4900742ade895b845cde5548f3
tomasz-herman <tomasz.herman@protonmail.com>
2019-12-23 20:53:57 +0100

    keep faulty line number in parse exception

--- src/com/hermant/parser/ParseException.java
+++ src/com/hermant/parser/ParseException.java
@@ -1,7 +1,14 @@
 package com.hermant.parser;
 
 public class ParseException extends Exception{
+    private int lineNumber;
+    
     ParseException(String errorMessage, int lineNumber) {
         super(errorMessage + " at line " + lineNumber);
+        this.lineNumber = lineNumber;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
     }
 }
