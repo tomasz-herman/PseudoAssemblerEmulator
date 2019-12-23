1a257fb892622521708029650faa05a284d69fff
tomasz-herman <tomasz.herman@protonmail.com>
2019-12-23 21:20:43 +0100

    getter for error message in ParseException.java

--- src/com/hermant/parser/ParseException.java
+++ src/com/hermant/parser/ParseException.java
@@ -2,13 +2,19 @@ package com.hermant.parser;
 
 public class ParseException extends Exception{
     private int lineNumber;
+    private String errorMessage;
 
     ParseException(String errorMessage, int lineNumber) {
         super(errorMessage + " at line " + lineNumber);
         this.lineNumber = lineNumber;
+        this.errorMessage = errorMessage;
     }
 
     public int getLineNumber() {
         return lineNumber;
+    }
+
+    public String getErrorMessage(){
+        return errorMessage;
     }
 }
