02993e63f538f7d33d96de40c2c2dee04e2c038d
tomasz-herman <tomasz.herman@protonmail.com>
2019-12-24 00:00:47 +0100

    parser fixes

--- src/com/hermant/parser/Parser.java
+++ src/com/hermant/parser/Parser.java
@@ -519,6 +519,7 @@ public class Parser {
         var iterator = lines.iterator();
         while (iterator.hasNext()) {
             String line = iterator.next();
+            lineNum++;
             line = removeComment(line);
             line = processLabels(line, labels, lineNum);
             if(line.isBlank())continue;
@@ -586,6 +587,7 @@ public class Parser {
             else throw new ParseException("illegal syntax: " + line, lineNum);
         else{
             words = splitByWhiteSpaces(line);
+            if(words.length > 2 && TOKENS.get(words[0]) != null) throw new ParseException("unknown token: " + words[0], lineNum);
             if(words.length > 2) throw new ParseException("illegal syntax: " + line + "\n(missing comma?)", lineNum);
         }
         words[0] = words[0].toUpperCase(Locale.ROOT);
